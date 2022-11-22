98735ba5cb2197b8b82caf163584d4345f0a91a7
ericksiqueiraa <erickpls06@gmail.com>
2022-11-22 20:15:38 -0300

    initial project v1

---
+++ src/components/TimeLocation.jsx
@@ -0,0 +1,19 @@
+const TimeLocation = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 31 may 2022 | Local time: 12:46 PM
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+          Curitiba, BR
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeLocation;
